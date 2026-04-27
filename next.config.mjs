d76d05408d384014c1f8755834aa2dd4f44de7aa
Emeka <njokubright08@gmail.com>
2026-04-27 10:04:07 +0100

    chore(next): update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-      },
-    ],
-  },
   webpack: (config, { isServer }) => {
     if (isServer) {
       config.externals.push('pdfkit');
     }
     return config;
-  },
-  experimental: {
-    instrumentationHook: true,
   },
 };
 
